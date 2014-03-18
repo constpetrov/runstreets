7f8f9a312bee7132ba3fab63ccb933f64f3d104a
constpetrov <constant.petrov@gmail.com>
2014-03-18 10:04:36 +0400

    dbhelper fix
    Signed-off-by: constpetrov <constant.petrov@gmail.com>

--- src/com/constpetrov/runstreets/StreetsDBHelper.java
+++ src/com/constpetrov/runstreets/StreetsDBHelper.java
@@ -1,6 +1,27 @@
 package com.constpetrov.runstreets;
 
-public class StreetsDBHelper extends SQLiteDBOpenHelper
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class StreetsDBHelper extends SQLiteOpenHelper
 {
+	private final static String DB_NAME = "runstreets";
+	private final static int version = 1;
+	public StreetsDBHelper(Context context){
+		super(context, DB_NAME, null, version);
+	}
+	
+	@Override
+	public void onCreate(SQLiteDatabase arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
