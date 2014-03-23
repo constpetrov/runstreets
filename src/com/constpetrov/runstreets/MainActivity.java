e2c3da4b7d6c2e8b2c8c34c7336642f87b8dfa90
costpetrov <constant.petrov@gmail.com>
2014-03-23 21:41:28 +0400

    add simple query interface

--- src/com/constpetrov/runstreets/MainActivity.java
+++ src/com/constpetrov/runstreets/MainActivity.java
@@ -1,12 +1,24 @@
 package com.constpetrov.runstreets;
 
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	private StreetsDataSource dataSource;
+	
+	Button execButton;
+	EditText queryText;
+	ListView resultView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +26,28 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		dataSource = new StreetsDataSource(this);
 		dataSource.checkAndCreate();
+		
+		queryText = (EditText) findViewById(R.id.editText1);
+		resultView = (ListView) findViewById(R.id.listView1);
+		execButton = (Button) findViewById(R.id.button1);
+		execButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				String query = queryText.getText().toString();
+				if(!"".equals(query)){
+					List<String> res = dataSource.execReadQuery(query);
+					
+					for(String str: res){
+						TextView child = new TextView(getApplicationContext());
+						child.setText(str);
+						resultView.addView(child);
+					}
+				}
+			}
+		});
+		
+		
 	}
 
 	@Override
