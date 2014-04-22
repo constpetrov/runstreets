ac24041f0978be92ece8323d7c67925db3d272ef
constpetrov <constant.petrov@gmail.com>
2014-04-22 16:02:16 +0400

    add Parcelable to Area
    Signed-off-by: constpetrov <constant.petrov@gmail.com>

--- src/com/constpetrov/runstreets/model/Area.java
+++ src/com/constpetrov/runstreets/model/Area.java
@@ -2,13 +2,71 @@ package com.constpetrov.runstreets.model;
 
 import java.util.StringTokenizer;
 
-public class Area {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Area implements Parcelable{
+	public static Parcelable.Creator<Area> CREATOR = new Parcelable.Creator<Area>() {
+
+		@Override
+		public Area createFromParcel(Parcel source) {
+			return new Area(source);
+		}
+
+		@Override
+		public Area[] newArray(int size) {
+			return new Area[size];
+		}
+	};
 	private int id;
 	private String code;
 	private int parentId;
 	private int type;
 	private String name;
 	private String doc;
+	
+	public Area(){};
+	
+	public Area(Parcel in){
+		id = in.readInt();
+		code = in.readString();
+		parentId = in.readInt();
+		type = in.readInt();
+		name = in.readString();
+		doc = in.readString();
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeInt(id);
+		dest.writeString(code);
+		dest.writeInt(parentId);
+		dest.writeInt(type);
+		dest.writeString(name);
+		dest.writeString(doc);
+	}
+
+	@Override
+	public String toString(){
+		return getAbbr();
+	}
+	
+	private String getAbbr() {
+		if(getName().startsWith("Зелен")){
+			return "Зеленоград";
+		}
+		StringTokenizer tokenizer = new StringTokenizer(getName(), " -");
+		StringBuilder b = new StringBuilder();
+		while (tokenizer.hasMoreTokens()){
+			b.append(tokenizer.nextToken().substring(0,1).toUpperCase());
+		}
+		return b.toString();
+	}
+
+	public String getDistrictName(){
+		return getName().replace("Район ", "").replace(" район", "");
+	}
+	
 	public int getId() {
 		return id;
 	}
@@ -47,24 +105,7 @@ public class Area {
 	}
 	
 	@Override
-	public String toString(){
-		return getAbbr();
+	public int describeContents() {
+		return 0;
 	}
-	
-	private String getAbbr() {
-		if(getName().startsWith("Зелен")){
-			return "Зеленоград";
-		}
-		StringTokenizer tokenizer = new StringTokenizer(getName(), " -");
-		StringBuilder b = new StringBuilder();
-		while (tokenizer.hasMoreTokens()){
-			b.append(tokenizer.nextToken().substring(0,1).toUpperCase());
-		}
-		return b.toString();
-	}
-	
-	public String getDistrictName(){
-		return getName().replace("Район ", "").replace(" район", "");
-	}
-	
 }
