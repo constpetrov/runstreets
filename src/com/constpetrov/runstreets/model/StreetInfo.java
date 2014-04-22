4bde7902338eb6c0eced3e2014b6213d093aa9e2
constpetrov <constant.petrov@gmail.com>
2014-04-22 16:54:08 +0400

    add Parcelable to StreetInfo
    Signed-off-by: constpetrov <constant.petrov@gmail.com>

--- src/com/constpetrov/runstreets/model/StreetInfo.java
+++ src/com/constpetrov/runstreets/model/StreetInfo.java
@@ -1,12 +1,51 @@
 package com.constpetrov.runstreets.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
-public class StreetInfo {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class StreetInfo implements Parcelable{
 	private Street street;
 	private String streetTypeName;
 	private List<Area> areas;
 	private List<StreetHistory> history;
+	
+	public static Parcelable.Creator<StreetInfo> CREATOR = new Parcelable.Creator<StreetInfo>() {
+
+		@Override
+		public StreetInfo createFromParcel(Parcel source) {
+			return new StreetInfo(source);
+		}
+
+		@Override
+		public StreetInfo[] newArray(int size) {
+			return new StreetInfo[size];
+		}
+	};
+
+	public StreetInfo(){};
+	
+	public StreetInfo(Parcel in){
+		street = in.readParcelable(null);
+		streetTypeName = in.readString();
+		areas = new ArrayList<Area>();
+		areas.addAll((Collection<? extends Area>) Arrays.asList(in.readParcelableArray(null)));
+		history = new ArrayList<StreetHistory>();
+		history.addAll((Collection<? extends StreetHistory>) Arrays.asList(in.readParcelableArray(null)));
+	}
+	
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeParcelable(street, 0);
+		dest.writeString(streetTypeName);
+		dest.writeParcelableArray(areas.toArray(new Area[areas.size()]), 0);
+		dest.writeParcelableArray(history.toArray(new StreetHistory[history.size()]), 0);
+	}
+	
 	public Street getStreet() {
 		return street;
 	}
@@ -30,6 +69,11 @@ public class StreetInfo {
 	}
 	public void setHistory(List<StreetHistory> history) {
 		this.history = history;
+	}
+	
+	@Override
+	public int describeContents() {
+		return 0;
 	}
 
 }
